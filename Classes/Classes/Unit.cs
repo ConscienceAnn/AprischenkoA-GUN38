21d3987c84b4958e10e6ce953ae67e795dc35e50
Conscience <whilstru@yandex.ru>
2025-08-11 13:43:26 +0300

    ДЗ по блоку Классы

---
+++ Classes/Classes/Unit.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace Classes
+{
+    public class Unit
+    {
+        public string Name { get; }
+        private float _health; //а не нужно устанавливать какое то значение? в таком формате будет по умолчанию 0, и тогда пациент скорее мертв.. а на старте то должен быть полон сил...
+
+
+        public Unit(string name)
+        {
+            Name = name;
+        }
+
+        public float Health => _health;
+
+        public int Damage { get; } = 5;
+        public float Armor { get; } = 0.6f;
+
+        public Unit() : this("Unknown Unit") { }
+
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor);
+        }
+
+        public bool SetDamage(float value) 
+        {
+            if (value < 0f) //вроде не требуется, но есть смысл добавить проверку значения value
+            {
+                return false; //не поняла только что возращать, вроде так игнорируется если будет отрицательное значение
+            }
+            else
+            {
+                _health -= value * Armor;
+                return _health <= 0f;
+            }
+        }
+    }
+}
---
+++ Classes/Classes/Weapon.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace Classes
+{
+    public class Weapon
+    {
+        public string Name { get; }
+        public int MinDamage { get; private set; }
+
+        public int MaxDamage { get; private set; }
+        public float Durability { get; } 
+
+        public Weapon(string name)
+        {
+            Name = name;
+            Durability = 1f;
+        }
+
+        public Weapon(string name, int minDamage, int maxDamage) : this(name)  
+        {
+            SetDamageParams(minDamage, maxDamage);  
+        }
+
+        public int GetDamage()
+        {
+            return (MinDamage + MaxDamage) / 2;  
+        }
+
+        public void SetDamageParams(int minDamage, int maxDamage)
+        {
+            // Проверка 1: min > max → меняем местами
+            if (minDamage > maxDamage)
+            {
+                (minDamage, maxDamage) = (maxDamage, minDamage); 
+                Console.WriteLine($"Некорректные входные данные для {Name}, минимальное значение больше максимального!");
+            }
+
+            // Проверка 2: min < 1 → ставим 1
+            if (minDamage < 1)
+            {
+                minDamage = 1;
+                Console.WriteLine($"Минимальный урон {Name} = 1.");
+            }
+
+            // Проверка 3: max <= 1 → ставим 10
+            if (maxDamage <= 1)
+            {
+                maxDamage = 10;
+                Console.WriteLine($"Максимальный урон {Name} = 10.");
+            }
+
+            // Устанавливаем значения
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+        }
+
+    }
+}
