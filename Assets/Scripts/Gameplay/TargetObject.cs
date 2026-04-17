b0ec75e7c372473971afb88e54945a9c80795d97
Conscience <whilstru@yandex.ru>
2026-04-17 15:09:51 +0300

    апгрейд визуала Level1

--- Assets/Scripts/Gameplay/TargetObject.cs
+++ Assets/Scripts/Gameplay/TargetObject.cs
@@ -1,4 +1,20 @@
+using UnityEngine;
+
 namespace Gameplay
 {
-    public class TargetObject : SceneObjectBase { }
+    public class TargetObject : SceneObjectBase 
+    {
+        private Quaternion _fixedRotation;
+
+        private void Start()
+        {
+            _fixedRotation = Quaternion.identity;
+        }
+
+        private void LateUpdate()
+        {
+            // Компенсируем вращение родителя
+            transform.rotation = _fixedRotation;
+        }
+    }
 }
