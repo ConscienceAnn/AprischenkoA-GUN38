94e877b68de0c5d3656303fa10e1d00e9bcc8c02
Conscience <whilstru@yandex.ru>
2026-06-03 11:06:03 +0300

    реализована отмена асинхронной операции

--- Assets/Scripts/Models/TimeModel.cs
+++ Assets/Scripts/Models/TimeModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Core.Utils;
 using Cysharp.Threading.Tasks;
 using Models.Interfaces;
@@ -6,19 +7,48 @@ using UniRx;
 
 namespace Models
 {
-    public sealed class TimeModel:  ITimeModel
+    public sealed class TimeModel : ITimeModel
     {
         private readonly ReactiveProperty<int> _gameTime = new();
-        public IObservable<int> GameTime =>  _gameTime;
+        private CancellationTokenSource _cts;
+        public IObservable<int> GameTime => _gameTime;
 
-        public void Initialize() => CountTime().Forget();
+        public void Initialize()
+        {
+            _cts = new CancellationTokenSource();  // создаем "пульт управления"
+            CountTime(_cts.Token).Forget();        // запускаем таймер с возможностью отмены
+        }
 
-        private async UniTask CountTime()
+        private async UniTask CountTime(CancellationToken token)
         {
             while (true)
             {
-                await UniTask.Delay(NumericConstants.One * 1000);
+                //если просят остановиться - выходим
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+                await UniTask.Delay(NumericConstants.One * 1000, cancellationToken: token);
+
+                // во время ожидания могли попросить остановиться
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                // Если дошли сюда - все ок, увеличиваем время
                 _gameTime.Value++;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Проверяем, существует ли "пульт управления"
+            if (_cts != null)
+            {
+                _cts.Cancel();   // Нажимаем кнопку "СТОП"
+                _cts.Dispose();  // Убираем батарейки (освобождаем ресурсы)
+                _cts = null;     // Забываем про пульт
             }
         }
     }
